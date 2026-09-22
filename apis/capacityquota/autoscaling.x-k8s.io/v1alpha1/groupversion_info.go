0c0ec9a2779a4278ce29165088ec51299d086721
William Yao <william2000yao@gmail.com>
2026-09-22 16:00:28 -0700

    fix: adapt Azure registration and generated API clients
    Expose bootstrap's generated API client and informer changes, scheme aliases, and unconditional Azure registration. No generated files are manually edited.

--- apis/capacityquota/autoscaling.x-k8s.io/v1alpha1/groupversion_info.go
+++ apis/capacityquota/autoscaling.x-k8s.io/v1alpha1/groupversion_info.go
@@ -32,6 +32,9 @@ var (
 	// GroupVersion is group version used to register these objects.
 	GroupVersion = schema.GroupVersion{Group: "autoscaling.x-k8s.io", Version: "v1alpha1"}
 
+	// SchemeGroupVersion is used by the generated client and apply-configuration packages.
+	SchemeGroupVersion = GroupVersion
+
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = runtime.NewSchemeBuilder(func(scheme *runtime.Scheme) error {
 		metav1.AddToGroupVersion(scheme, GroupVersion)
